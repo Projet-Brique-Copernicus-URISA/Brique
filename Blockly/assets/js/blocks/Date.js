0c6847dc5d628a80f683614e16cd0f324524ec22
Noa Ammirati <noa.ammirati@gmail.com>
2020-06-30 12:21:29 +0200

    date terminee

--- Blockly/assets/js/blocks/Date.js
+++ Blockly/assets/js/blocks/Date.js
@@ -11,11 +11,11 @@ Blockly.Blocks['block_date'] = {
     init: function () {
         this.appendDummyInput()
             .appendField("Date :")
-            .appendField(new Blockly.FieldNumber(0, 1, 31), "DAY")
+            .appendField(new Blockly.FieldNumber(new Date().getDate(), 1, 31), "DAY")
             .appendField("/")
-            .appendField(new Blockly.FieldNumber(0, 1, 12), "MONTH")
+            .appendField(new Blockly.FieldNumber(new Date().getMonth() + 1, 1, 12), "MONTH")
             .appendField("/")
-            .appendField(new Blockly.FieldNumber(0, 2018, 2020), "YEAR");
+            .appendField(new Blockly.FieldNumber(new Date().getFullYear(), 2010, new Date().getFullYear()), "YEAR");
         this.setOutput(true, "date");
         this.setColour(75);
         this.setTooltip("JJ/MM/AAAA");
@@ -23,23 +23,16 @@ Blockly.Blocks['block_date'] = {
     }
 };
 
+
+/**/
+
 Blockly.JavaScript['block_date'] = function (block) {
     var number_day = block.getFieldValue('DAY');
-    var number_month = block.getFieldValue('MONTH');
+    var number_month = block.getFieldValue('MONTH') - 1;
     var number_year = block.getFieldValue('YEAR');
-    // TODO CHECK IF IT4S A REAL DAY
-    var code = "new DateValue(" + number_day + "," + number_month + "," + number_year + ")";
+    var code = "new Date(" + number_year + "," + number_month + "," + number_day + ")";
     return [code, Blockly.JavaScript.ORDER_NONE];
 };
-
-class DateValue {
-    constructor(day, month, year) {
-        this.day = day;
-        this.month = month;
-        this.year = year;
-    }
-}
-
 
 Blockly.Blocks['block_period'] = {
     init: function () {
@@ -62,15 +55,21 @@ Blockly.Blocks['block_period'] = {
 Blockly.JavaScript['block_period'] = function (block) {
     var value_start = Blockly.JavaScript.valueToCode(block, 'START', Blockly.JavaScript.ORDER_ATOMIC);
     var value_end = Blockly.JavaScript.valueToCode(block, 'END', Blockly.JavaScript.ORDER_ATOMIC);
-    // TODO: CHECK IF THE START IS BEFORE THE END
-    var code = "new PeriodValue(" + value_start + "," + value_end + ")";
-    // TODO: Change ORDER_NONE to the correct strength.
+    var code = "doPeriod(" + value_start + "," + value_end + ")";
     return [code, Blockly.JavaScript.ORDER_NONE];
 };
 
-class PeriodValue{
-    constructor(start, end){
-        this.start= start;
+function doPeriod(value_start, value_end){
+    if (value_start.getTime() > value_end.getTime()) {
+        alert("Période non conforme : " + value_start.toLocaleDateString() + " n'est pas antérieure à " + value_end.toLocaleDateString());
+    } else {
+        return new PeriodValue(value_start, value_end);
+    }
+}
+
+class PeriodValue {
+    constructor(start, end) {
+        this.start = start;
         this.end = end;
     }
 }
