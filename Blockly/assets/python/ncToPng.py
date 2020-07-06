bc53c7974a3da37e22f0e2b5d972deab268f79b7
Maxime Dumonteil <maxime.dumonteil@etu.u-bordeaux.fr>
2020-07-06 19:21:50 +0200

    add parameters to ncToPng.py

--- Blockly/assets/python/ncToPng.py
+++ Blockly/assets/python/ncToPng.py
@@ -3,16 +3,24 @@ import numpy as np
 import matplotlib.pylab as plt
 from matplotlib import cm
 from mpl_toolkits.basemap import Basemap,shiftgrid
+import sys, os
+
+# first arg is the original netCDF file to convert
+# the second arg is the variable's name in the netCDF file
+# the third arg is the name of the graph
+# the fourth arg is the name of the created png file
+
 
 # set up the figure
 plt.figure()
 
 #Get value from nc file
-url='download.nc'
+url = sys.argv[1] #url='download.nc'
 file = netCDF4.Dataset(url)
 lat  = file.variables['latitude'][:]
 lon  = file.variables['longitude'][:]
-data = file.variables['gtco3'][0,:,:]
+#data = file.variables['gtco3'][0,:,:]
+data = file.variables[sys.argv[2]][0,:,:]
 
 #Setup figure and map
 fig=plt.figure(figsize=(12, 8))
@@ -37,7 +45,9 @@ m.drawparallels(np.arange(-90.,90.,30.),labels=[1,0,0,0])
 m.drawmeridians(np.arange(-180.,180.,60.),labels=[0,0,0,1])
 
 # Add a colorbar and title, and then show the plot.
-plt.title('titre');
+#plt.title('titre');
+plt.title(sys.argv[3]);
 
 #plt.show();
-plt.savefig('image.png', bbox_inches=0)
+#plt.savefig('image.png', bbox_inches=0)
+plt.savefig(sys.argv[4], bbox_inches=0)
