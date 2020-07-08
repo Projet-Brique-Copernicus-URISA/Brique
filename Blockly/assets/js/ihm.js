c72799c7d072ecf618e0f54cc914485911cedf13
Noa Ammirati <noa.ammirati@gmail.com>
2020-07-08 18:22:43 +0200

    plein ecran ok

--- Blockly/assets/js/ihm.js
+++ Blockly/assets/js/ihm.js
@@ -14,8 +14,12 @@
  * @version 0.1
  */
 
+var zoom;
+
 /** Initialise the ihm interface actions */
 $(document).ready(function () {
+
+    zoom = false;
 
     $('.button-info').hover(
         function () {
@@ -53,11 +57,64 @@ $(document).ready(function () {
 
     $('#button-max').click(
         function () {
-            alert("Zoom");
+            doZoom();
         }
     );
 
     $('#canvas-display').dblclick(function () {
-        alert("Zoom");
+        doZoom();
     });
+});
+
+function doZoom() {
+    if (isInMax()){
+        minimize();
+    } else {
+        maximize();
+    }
+}
+
+function maximize() {
+    var elem = document.getElementById('element-right-pan');
+    if (elem.requestFullscreen) {
+        elem.requestFullscreen();
+    } else if (elem.mozRequestFullScreen) { /* Firefox */
+        elem.requestFullscreen();
+    } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
+        elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) { /* IE/Edge */
+        elem.msRequestFullscreen();
+
+    }
+}
+
+function minimize() {
+    if (document.exitFullscreen) {
+        document.exitFullscreen();
+    } else if (document.mozCancelFullScreen) {
+        document.mozCancelFullScreen();
+    } else if (document.webkitExitFullscreen) {
+        document.webkitExitFullscreen();
+    } else if (document.msExitFullscreen) {
+        document.msExitFullscreen();
+    }
+}
+
+function isInMax() {
+    var full_screen_element = document.fullscreenElement || document.webkitFullscreenElement || document.mozFullScreenElement || document.msFullscreenElement || null;
+    if (full_screen_element === null)
+        return false;
+    else
+        return true;
+}
+
+$(document).on('fullscreenchange webkitfullscreenchange mozfullscreenchange MSFullscreenChange', function() {
+	if(isInMax()) {
+        $('#canvas-display').css("height", "1000px");
+        $('#button-min-max').attr("src", "assets/img/minimize.png");
+	}
+	else {
+        $('#canvas-display').css("height", "500px");
+        $('#button-min-max').attr("src", "assets/img/maximize.png");
+	}
 });
