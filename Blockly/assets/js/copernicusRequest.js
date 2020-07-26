a0f43a2b212d84f01abcd8ea7f55b0837ea5b96d
Maxime Dumonteil <maxime.dumonteil@etu.u-bordeaux.fr>
2020-07-26 19:45:14 +0200

    modify copernicus request

--- Blockly/assets/js/copernicusRequest.js
+++ Blockly/assets/js/copernicusRequest.js
@@ -67,22 +67,6 @@ var createCopernicusRequest = function (topic, date, area){
     var req_otherContent = '';
     var api_origin = '';
 
-    //add copyright to the computed file
-    var req_copyright = "\"\"\" \t" 
-        + "Copyright 2020 Noa Ammirati Maxime Dumonteil Mathis Lecoeuvre URISA\t\t"
-        + "This file is part of Brique Copernicus.\t"
-        + "This program is free software: you can redistribute it and/or modify\t"
-        + "it under the terms of the GNU General Public License as published by\t"
-        + "the Free Software Foundation, either version 3 of the License, or\t"
-        + "(at your option) any later version.\t\t"
-        + "This program is distributed in the hope that it will be useful,\t"
-        + "but WITHOUT ANY WARRANTY; without even the implied warranty of\t"
-        + "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\t"
-        + "GNU General Public License for more details.\t\t"
-        + "You should have received a copy of the GNU General Public License\t"
-        + "along with this program.  If not, see <https://www.gnu.org/licenses/>.\t"
-        + "\"\"\"\t\t";
-
     //switch to get correct real request's parameters according to the topic
     switch (topic){
         case 'thematic_atmosphere_pollution_particulate':
@@ -194,7 +178,7 @@ var createCopernicusRequest = function (topic, date, area){
     //add clean part of the script
     scriptContent_process += createContentForCleanPart();
 
-    return req_copyright + req_final + "\n\n" + scriptContent_process;
+    return req_final + "\n\n" + scriptContent_process;
 }
 
 /**
